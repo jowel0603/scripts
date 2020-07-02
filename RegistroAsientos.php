f8618d2db80f135736dff88e3f914f9d688b4425
Joel Valencia <32463780+jowel0603@users.noreply.github.com>
2020-07-02 16:03:17 -0500

    Add files via upload

---
+++ RegistroAsientos.php
@@ -0,0 +1,377 @@
+<?php require_once '../include/head/head.php'; 
+
+if ($_SESSION["Asientos Contables"] == 1 ) {
+
+
+?>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+
+  	<!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="../cpanel/cpanel.php">Home</a></li>
+              <li class="breadcrumb-item active">DataTables</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+
+      <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <!-- SELECT2 EXAMPLE -->
+        <div class="card card-default">
+          <div class="card-header">
+            <h3 class="card-title">Select2 (Default Theme)</h3>
+
+            <div class="card-tools">
+              <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i></button>
+              <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-remove"></i></button>
+            </div>
+          </div>
+          <!-- /.card-header -->
+          <div class="card-body">
+            <div class="row">
+
+	            <div id="msg">
+	            	
+	            </div>
+
+
+	            <form id="addAsientoscontables" name="addAsientoscontables"  method="POST" class="form-horizontal" >
+				<input type="hidden" class="form-control" id="idusuario" name="idusuario" value="<?php echo $_SESSION["id"];  ?>" >
+
+
+						<div class="row">
+
+							
+						  
+
+							<div class="form-group col-md-3">
+                              <label>Fecha: *</label>
+                                <input type="date" name="fecha" id="fecha" class="form-control">
+                            </div>
+
+
+                           
+
+                           <div class="form-group col-md-4">
+                              <label>Diarios: *</label>
+                                <select class="form-control select2" name="idtipodiario" id="idtipodiario" required  style="width: 100%;">
+                        			<option  >-----------</option>
+                          			<?php
+
+
+		                            require_once '../../modelos/Diarios/mdlDiarios.php';
+
+		                            $mdlDiarios = new ModelDiarios();
+		                            $listar = $mdlDiarios->tipoDiarios();
+
+
+                          			foreach ($listar as $key => $data3) {
+                                      # code...
+                                      # 
+                          				echo '<option value="'.$data3["idtipod"].'">'.$data3["nombre"].'</option>';
+
+                                      }
+                                	?>
+                      			</select>
+                            </div>
+
+
+                            <div class="form-group col-md-4">
+                              <label>Periodo : *</label>
+                                <select class="form-control select2" name="idtipoperiodo" id="idtipoperiodo" required  style="width: 100%;">
+                        			<option> ----------- </option>
+                          			<?php
+
+
+		                            require_once '../../modelos/asientoscontables/mdlAsientoscontables.php';
+
+		                            $mdlAsientosperiodos = new ModeloAsientos();
+		                            $listar = $mdlAsientosperiodos->Listarperiodo();
+
+
+                          			foreach ($listar as $key => $data3) {
+                                      # code...
+                                      # 
+                          				echo '<option value="'.$data3["idperiodo"].'">'.$data3["Codigo"].'</option>';
+
+                                      }
+                                	?>
+                      			</select>
+                            </div>
+							
+						</div>
+							
+
+
+						<div class="row">
+
+							<div class="form-group col-md-3">
+                              <label>Rut: *</label>
+                                <input type="text" name="nit" id="nit" class="form-control" autocomplete="off" placeholder="Rut de la Empresa" oninput="checkRut(this)"  >
+                                <input type="hidden" name="idempresa" id="idempresa">
+                            </div>
+
+
+
+                            <div class="form-group col-md-4">
+                              <label>Empresa: *</label>
+                                <input type="text" name="nombrempresa" id="nombrempresa" class="form-control" autocomplete="off" placeholder="Nombre Empresa" readonly="">
+                            </div>
+
+
+
+                           <div class="form-group col-md-4">
+                              <label>Estado: *</label>
+                                <select class="form-control select2" id="estado" name="estado" style="width: 100%;">
+                                <option selected="selected">Sleccione un Estado</option>
+                                <option selected="true" value="Abierto">Abierto</option>
+                              </select>
+                            </div>
+							
+						</div>
+
+						<br><br><br>
+
+						<div class="row">
+							<div class="form-group col-md-6">
+                              <label>Concepto: *</label>
+                                <input type="text" name="concepto" id="concepto" class="form-control" autocomplete="off" placeholder="Digite un concepto">
+                            </div>
+						
+
+							<div class="form-group col-md-5">
+                              <label>Referencia: *</label>
+                                <input type="text" name="referencia" id="referencia" class="form-control" autocomplete="off" placeholder="Digite un concepto">
+                        	</div>
+                        </div>
+
+
+						<div id="mostrarDatos" style="color:green;" >
+                           
+                          </div>
+                          <div id="error" name="error" style="color:red">
+                            <!-- error will be shown here ! -->
+                          </div> 
+
+						
+							
+						
+							
+
+						<br><br>
+
+
+
+							
+						
+					  	<table id="asientoscontablesTable" class="table ">
+			                <thead>
+			                <tr>
+			                  <th>#</th>
+			                  <th>Codigo</th>
+			                  <th>Nombre Cuenta</th>
+			                  <th>Debe</th>
+			                  <th>Haber</th>
+			                  <th>Accion</th>
+			                </tr>
+			                </thead>
+			                <tbody>
+			                <!--Comenzamos a añadir campos para venta -->
+			                <?php
+			                $arrayNumber = 0;
+			                #contador para añadir lineas
+			                $numero = 1;
+			                $x=1;
+			                for ($x=0; $x < 4; $x++) { 
+			                 ?>
+
+			                  <!--Se imprime las filas en la tabla -->
+			                  <tr id="row<?php echo $x; ?>" class="<?php echo $arrayNumber; ?>">
+
+			                    <td style="width:7%;">
+			                    <input type="text" name="num[]" id="num<?php echo $x; ?>" autocomplete="off"  class="form-control"  value="<?php echo $numero; ?>" disabled="true"/>  
+			                    
+			                  </td>
+
+			                  <td style="margin-left:20px;">
+			                    <input type="text" name="codigocuenta[]" id="codigocuenta<?php echo $x; ?>" autocomplete="off"  class="form-control" onchange="getCodigocuenta(<?php echo $x; ?>)"  /> 
+			                    <input type="hidden" name="n_digitos" id="n_digitos" > 
+			                  </td>
+			                  
+			                 
+			                  
+			                    <td style="padding-left:20px;">
+			                      <div class="form-group">
+			                        <input type="text" name="nombrecuenta[]" id="nombrecuenta<?php echo $x; ?>" autocomplete="off"  class="form-control" />  
+			                      </div>
+			                    </td>
+
+			                    <td style="padding-left:20px;">
+			                      <input type="text" name="debe[]" id="debe<?php echo $x; ?>" autocomplete="off"  class="form-control" onkeypress="return valideKey(event);"  onkeyup="sumarsubtotal(<?php echo $x; ?>)"; />  
+			                    </td>
+
+			                    <td style="padding-left:20px;">
+			                      <div class="form-group">
+			                      <input type="text" name="haber[]" id="haber<?php echo $x; ?>"  autocomplete="off" class="form-control" onkeypress="return valideKey(event);"    />
+			                      </div>
+			                    </td> 
+
+			                   
+
+			                    <td>
+			                      <button class="btn btn-default removeProductRowBtn" type="button" id="removeProductRowBtn" onclick="removeProductRow(<?php echo $x; ?>)"><i class="far fa-trash-alt"></i></button>
+			                    </td>  
+
+			                    <td>
+			                    	<input type="text" name="subtotal" id="subtotal<?php echo $x; ?>"  autocomplete="off" class="form-control"  onchange="sumarsubtotal(<?php echo $x; ?>);"  />
+			                    	
+			                  </tr>
+
+			                 
+
+			                  
+
+
+			                <?php
+			                $arrayNumber++;
+			                $numero++;
+			                $x++;
+			                }//for donde añadimos los campos
+			                ?>
+			         	  </tbody>
+			                
+			              </table>
+
+			              <div  class="row">
+
+			              	<table>
+			              		<tr>
+			              			<td id="total_debe">Total Debe</td>
+			              			<td><input  type="text" name="totaldebe" id="totaldebe" class="form-control"  readonly=""></td>
+			              			
+			              		</tr>
+
+			              		<tr>
+			              			<td id="total_haber">Total Haber</td>
+			              			<td><input type="text" name="totalhaber" id="totalhaber" class="form-control" readonly=""></td>
+			              		</tr>
+			              	</table>
+			              	
+			              </div>
+
+			             
+
+
+
+			              
+
+			              <br><br>
+
+
+
+			              <div style="padding-top:20px;" class="form-group submitButtonFooter">
+						    <div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+						   	<!-- accion para añadir td a la atabla -->
+
+						   	<button  type="button" class="btn  btn-dark "  id="addRowBtn" data-loading-text="Cargando..."> <i class="glyphicon glyphicon-plus-sign"></i>Añadir Fila</button>
+						    
+
+						    <button class="btn btn-primary btnGuardarAsientoscontables" type="submit" ><i class="fa fa-save"></i>  Guardar</button>
+
+
+                      		<button class="btn btn-danger" onclick="cancelarform()" type="button"><i class="fa fa-arrow-circle-left"></i> Cancelar</button>
+						    <!-- fin accion -->
+
+							    
+						    </div>
+						
+						</div>
+
+
+
+			              </form>
+
+
+
+						</div>
+					</div>				
+		          </div>
+		      </div>
+		  
+	</section>
+		                
+
+
+ </div>
+
+
+ <?php 
+
+
+}else{
+	include '../include/noacceso.php';
+}
+
+?>
+<?php
+require_once '../include/footer/footer.php';
+ ?>
+
+ <script src="../scripts/asientoscontables.js"></script> 
+
+ <script>
+ 	$(function(){
+
+
+ 		$("#nit" ).autocomplete({
+            source: function( request, response ) {
+                
+                $.ajax({
+                    url: "../../controlador/empresa/Buscarempresa.php",
+                    type: 'post',
+                    dataType: "json",
+                    data: {
+                        search: request.term
+                    },
+                    success: function( data ) {
+                        response( data );
+                    }
+                });
+            },
+            select: function (event, ui) {
+	            $('#nombrempresa').val(ui.item.label); // display the selected text
+                $('#idempresa').val(ui.item.value); // save selected id to input
+	         
+                return false;
+            }
+        });
+
+
+   
+
+
+        	
+        	
+ 	});
+ </script>
+
+
+ 
+
+
+
+ 
---
+++ asientoscontables.js
@@ -0,0 +1,472 @@
+$(function(){
+
+
+ 
+
+
+  
+
+    
+	$('#addRowBtn').on('click', function(evt){
+		evt.preventDefault();
+
+		agregarFila();
+	});
+
+
+	load(1);
+
+
+
+
+	$('.btnGuardarAsientoscontables').on('click', function(evt){
+		evt.preventDefault();
+
+		let form = $('#addAsientoscontables').serialize();
+
+
+		$.ajax({
+			url: '../../controlador/asientoscontables/RegistroAsientosContables.php',
+			type: 'POST',
+			data: form,
+			dataType: 'html',
+			cache:false,
+			async:true,
+			contentType: false,
+			processData: false,
+
+			beforeSend: function(obj){
+				$('#msg').html('<div class="alert alert-info alert-dismissible fade show" role="alert">'+
+  											'<strong>Información!  </strong>Enviando datos, porfavor espere un momento.'+
+				  							'<button type="button" class="close" data-dismiss="alert" aria-label="Close">'+
+				    							'<span aria-hidden="true">&times;</span>'+
+				  							'</button>'+
+											'</div>');
+			},
+
+			success: function(datos){
+
+				if(datos == 'ok'){
+							$('#msg').html('<div class="alert alert-success alert-dismissible fade show" role="alert">'+
+  											'<strong>Exito!</strong> Registrado Diario correctamente.'+
+				  							'<button type="button" class="close" data-dismiss="alert" aria-label="Close">'+
+				    							'<span aria-hidden="true">&times;</span>'+
+				  							'</button>'+
+											'</div>');
+							limpiarcampos();
+							load(1);
+
+							
+						}else if(datos == 'error'){
+							$('#msg').html('<div class="alert alert-danger alert-dismissible fade show" role="alert">'+
+  											'<strong>ERROR!</strong> Error al ingresar los datos.'+
+				  							'<button type="button" class="close" data-dismiss="alert" aria-label="Close">'+
+				    							'<span aria-hidden="true">&times;</span>'+
+				  							'</button>'+
+											'</div>');
+						}else if(datos == 'error2'){
+							$('#msg').html('<div class="alert alert-danger alert-dismissible fade show" role="alert">'+
+  											'<strong>ERROR!</strong>Ya hay un Registro de Diario con este Codigo.'+
+				  							'<button type="button" class="close" data-dismiss="alert" aria-label="Close">'+
+				    							'<span aria-hidden="true">&times;</span>'+
+				  							'</button>'+
+											'</div>');
+						}else if(datos == '9'){
+							$('#msg').html('<div class="alert alert-danger alert-dismissible fade show" role="alert">'+
+  											'<strong>ERROR!</strong> Todos los campos son obligatorios.'+
+				  							'<button type="button" class="close" data-dismiss="alert" aria-label="Close">'+
+				    							'<span aria-hidden="true">&times;</span>'+
+				  							'</button>'+
+											'</div>');
+						}else if(datos == "8"){
+							$('#msg').html('<div class="alert alert-danger alert-dismissible fade show" role="alert">'+
+  											'<strong>ERROR!</strong> Caracteres mayor de 10 .'+
+				  							'<button type="button" class="close" data-dismiss="alert" aria-label="Close">'+
+				    							'<span aria-hidden="true">&times;</span>'+
+				  							'</button>'+
+											'</div>');
+						}
+
+			}
+		});
+	});
+
+
+
+	
+
+
+});
+
+/*
+function addRow(){
+	let tableLength = $("#asientoscontablesTable tbody tr").length;
+
+	let tableRow;
+	let arrayNumber;
+	let count;
+
+	if (tableLength > 0) {
+		tableRow = $("#asientoscontablesTable tbody tr:last").attr('id');
+		arrayNumber = $("#asientoscontablesTable tbody tr:last").attr('class');
+		count = tableRow.substring(3);
+		count = Number(count) + 1;
+		arrayNumber = Number(arrayNumber) + 1;
+	}else{
+		count = 1;
+		arrayNumber = 0;
+	}
+
+	$.ajax({
+		url: '../../controlador/asientoscontables/FetchAsientosData.php',
+		type: 'post',
+		dataType: 'json',
+		success:function(response) {
+			$("#addRowBtn").button("reset");
+
+			let tr = '<tr id="row'+count+'" class="'+arrayNumber+'">'+
+					
+					'<td style="margin-left:20px;"">'+
+						'<input type="text" name="codProd[]" id="codProd'+count+'" autocomplete="off"  class="form-control"  />'+
+					'</td style="padding-left:20px;">'+
+					
+					'<td style="display:none">'+
+						'<input type="text" name="statusProd[]" id="statusProd'+count+'" autocomplete="off"  class="form-control" style="display:none"  />'+
+					'</td>'+
+				
+					'<td style="padding-left:20px;"">'+
+					'<div class="form-group">'+
+					'<input type="text" class="form-control" name="productName[]" id="productName'+count+'" disabled="true" >'+
+					
+					'</div>'+
+					'</td>'+
+				
+					'<td style="padding-left:20px;"">'+
+						'<input type="text" name="rate[]" id="rate'+count+'" autocomplete="off" disabled="true" class="form-control" />'+
+						'<input type="hidden" name="rateValue[]" id="rateValue'+count+'" autocomplete="off" class="form-control" />'+
+					'</td style="padding-left:20px;">'+
+					
+					'<td style="padding-left:20px;">'+
+						'<div class="form-group">'+
+							'<input type="number" name="quantity[]" id="quantity'+count+'"  autocomplete="off" class="form-control" min="1" />'+
+						'</div>'+
+					'</td>'+
+				
+					'<td style="padding-left:20px;">'+
+						'<input type="text" name="total[]" id="total'+count+'" autocomplete="off" class="form-control" disabled="true" />'+
+						'<input type="hidden" name="totalValue[]" id="totalValue'+count+'" autocomplete="off" class="form-control" />'+
+					'</td>'+
+					
+					'<td>'+	
+						'<button class="btn btn-default removeProductRowBtn" type="button" onclick="removeProductRow('+count+')"><i class="glyphicon glyphicon-trash"></i></button>'+
+					'</td>'+
+					'</tr>';
+
+			if(tableLength > 0) {							
+				$("#asientoscontablesTable tbody tr:last").after(tr);
+			} else {				
+				$("#asientoscontablesTable tbody").append(tr);
+			}
+
+		} 
+	}); 
+}
+*/
+
+
+function load(page){
+
+
+
+	let query=$("#q").val();
+	let per_page=5;
+	let parametros = {"action":"ajax","page":page,'query':query,'per_page':per_page};
+	$("#loader").fadeIn('slow');
+
+			
+	$.ajax({
+		url:'../../controlador/asientoscontables/ListPuc.php',
+		cache: false,
+		asyn:true,
+		data: parametros,
+		
+		beforeSend: function(objeto){
+
+		 //$("#loader").html('<div class="alert-success">Cargando...</div>');
+		},
+		success:function(data){
+		 $(".outer_div").html(data).fadeIn('slow');
+		 $("#loader").html("");
+		}
+	})
+}
+
+let debe = 0;
+let subtotal = 0;
+///funcion añadir fila a la tabla
+function agregarFila(){
+
+	let tableLength = $("#asientoscontablesTable tbody tr").length;
+
+	let tableRow;
+	let arrayNumber;
+	let count;
+
+	if (tableLength < 6) {
+		tableRow = $("#asientoscontablesTable tbody tr:last").attr('id');
+		arrayNumber = $("#asientoscontablesTable tbody tr:last").attr('class');
+		count = tableRow.substring(3);
+		count = Number(count) + 1;
+		arrayNumber = Number(arrayNumber) + 1;
+	}else{
+		count = 1;
+		arrayNumber = 0;
+	}
+
+	$.ajax({
+		url: '../../controlador/asientoscontables/FetchAsientosData.php',
+		type: 'post',
+		dataType: 'json',
+		success:function(response) {
+
+
+		let fila = '<tr id="row'+count+'" class="'+arrayNumber+'">'+
+					'<td style="width:7%;">'+count+'</td>'+
+					'<td style="margin-left:20px;"><input type="text" name="codigocuenta[]" id="codigocuenta'+count+'" autocomplete="off"  class="form-control"  value="" onchange="getCodigocuenta('+count+')"  /></td>'+
+					'<td style="margin-left:20px;"><input type="text" name="nombrecuenta[]" id="nombrecuenta'+count+'" autocomplete="off"  class="form-control"  value="" /></td>'+
+					'<td style="padding-left:20px;"><input type="text" name="debe[]"  id="debe'+count+'" autocomplete="off"  class="form-control" onkeyup="sumarsubtotal('+count+')" onkeypress="return valideKey(event);"  /></td>'+
+					'<td style="padding-left:20px;"><input type="text" name="haber[]" id="haber'+count+'" autocomplete="off"  class="form-control"  value="" onkeypress="return valideKey(event);" onkeyup="format(this);" /></td>'+
+					'<td style="padding-left:20px;">'+
+					'<button class="btn btn-default removeProductRowBtn" type="button" id="removeProductRowBtn" onclick="removeProductRow('+count+')"><i class="far fa-trash-alt"></i></button></td>'+
+					'<td><input type="hidden" class="form-control" name="subtotal[]"  onchage="sumarsubtotal('+count+')"   id="subtotal'+count+'"  >'+
+				'</tr>';
+
+
+			if(tableLength > 0) {							
+				$("#asientoscontablesTable tbody tr:last").after(fila);
+			} else {				
+				$("#asientoscontablesTable tbody").append(fila);
+			}
+
+
+			
+
+
+			} // /success
+	}); // get the product data ajax
+	
+}
+
+
+
+//suma de subtotal por cada producto añadido
+function sumarsubtotal(){
+  
+  let debe=document.getElementsByName("debe[]");
+  let sub=document.getElementsByName("subtotal[]");
+
+  const total=0.0;
+
+  for (var i = 0; i < debe.length; i++) {
+
+  	let inpC=debe[i];
+  	let inpS=sub[i];
+
+    
+    inpC.value  = parseInt(inpC.value);
+    document.getElementsByName("subtotal")[i].innerHTML=inpC.value;
+  }
+	calcularDebe();
+  }
+
+//Eliminar Producto registro
+function removeProductRow(row = null){
+	if (row) {
+		$("#row"+row).remove();
+
+		
+	}else{
+		alert('Error !! Actualizar la Pagina (F5)');
+	}
+}
+//End Eliminar Producto registro
+
+
+
+
+
+
+//  //Funcion GETempleado
+function getCodigocuenta(row = null){
+
+	let codigocuenta = $("#codigocuenta"+row).val();
+
+		
+		
+
+	$.ajax({
+		url: '../../controlador/asientoscontables/Buscarplancuentas.php',
+		type: 'post',
+		data: {codigocuenta : codigocuenta},
+		dataType: 'json',
+		success:function(response) {
+					
+			$('#idc'+row).val(response.idc);
+			$('#codigocuenta'+row).val(response.n_digitos);
+			$('#nombrecuenta'+row).val(response.nombre);
+					
+		}//Success ajax
+			
+	});// /ajax function to fetch the product data	
+		
+		
+}
+///End GETempleado
+///
+
+
+
+
+//funcion de RUT Cliente
+function focusRutCliente()
+{
+    document.getElementById("nit").focus();
+}
+
+function checkRut(rut) 
+{
+
+  if (rut.value.length <= 1) {
+    mostrarDatos.classList.remove('alert-success', 'alert-danger');
+    console.log(rut);
+  }
+
+  // Obtiene el valor ingresado quitando puntos y guiÃ³n.
+  var valor = clean(rut.value);
+
+  // Divide el valor ingresado en dÃ­gito verificador y resto del RUT.
+  cuerpo = valor.slice(0, -1);
+  dv = valor.slice(-1).toUpperCase();
+
+  // Separa con un GuiÃ³n el cuerpo del dÃ­gito verificador.
+  rut.value = format(rut.value);
+
+  // Si no cumple con el mÃ­nimo ej. (n.nnn.nnn)
+  if (cuerpo.length < 7) {
+    rut.setCustomValidity("RUT Incompleto");
+    console.clear();//FRONT-END
+    return false;
+  }
+
+  // Calcular digito Verificador "metodo del MÃ³dulo 11"
+  suma = 0;
+  multiplo = 2;
+
+  // Para cada digito del Cuerpo
+  for (i = 1; i <= cuerpo.length; i++) {
+    // Obtener su Producto con el MÃºltiplo Correspondiente
+    index = multiplo * valor.charAt(cuerpo.length - i);
+
+    // Sumar al Contador General
+    suma = suma + index;
+
+    // Consolidar Multiplo dentro del rango [2,7]
+    if (multiplo < 7) {
+      multiplo = multiplo + 1;
+    } 
+  else 
+  {
+        multiplo = 2;
+    }
+    
+    
+  }
+
+  // Calcular Digito Verificador en base al Modulo 11
+  dvEsperado = 11 - (suma % 11);
+
+ // Casos Especiales (0 y K)
+    dv = (dv == 'K')?10:dv;
+    dv = (dv == 11)?0:dv;
+  
+
+  // Validar que el Cuerpo coincide con su DÃ­gito Verificador
+  if (dvEsperado != dv) {
+    rut.setCustomValidity('');
+    $('#mostrarDatos').html('Rut Ingresado No es Valido').css('class="alert alert-success  alert-dismissible  " ');
+    console.clear();
+    return false;
+  } 
+  else 
+  {
+
+      rut.setCustomValidity('');
+      $('#mostrarDatos').html('Rut Ingresado  es Valido').css('class="alert alert-success  alert-dismissible  " ');
+      console.clear();//FRONT-END
+      return true;
+   
+  }
+}
+
+function format (rut) {
+  rut = clean(rut)
+
+  var result = rut.slice(-4, -1) + '-' + rut.substr(rut.length - 1)
+  for (var i = 4; i < rut.length; i += 3) {
+    result = rut.slice(-3 - i, -i) + '.' + result
+  }
+
+  return result
+}
+
+function clean (rut) {
+  return typeof rut === 'string'
+    ? rut.replace(/^0+|[^0-9kK]+/g, '').toUpperCase()
+    : ''
+}
+
+///funcion para validar input que permita solo numeros asi no dejamos que el usuario digite numeros negativos
+function valideKey(evt) {
+    let code = evt.which ? evt.which : evt.keyCode;
+         if (code == 8) {
+                    //backspace
+             return true;
+          } else if (code >= 48 && code <= 57) {
+                    //is a number
+             return true;
+          } else {
+             return false;
+       }
+ }
+
+
+
+
+//aqui voy sumando los subtotales de todos los productos añadidos
+
+function calcularDebe(){
+
+  const sub = document.getElementsByName("subtotal[]");
+  let total=0.0;
+
+  for (let i = 0; i < sub.length; i++) {
+
+  	
+  		total += parseInt(sub[i].value);
+  	
+    
+      
+
+    
+
+  }
+
+
+  //$("#total_debe").html(total);
+  $("#totaldebe").val(total);
+
+  
+}
+
+
+
